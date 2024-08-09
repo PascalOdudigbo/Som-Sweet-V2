380a30e2f11e29e97c796d0b5bf3d42fbfba457e
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-08-09 23:26:40 +0100

    moving the prisma client database folder out to the src folder and setting up the project assets

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+export {default as searchIcon} from "./icons/searchIcon.png"
+export {default as profileIcon} from "./icons/profileIcon.png"
+export {default as downIcon} from "./icons/downIcon.png"
+export {default as upIcon} from "./icons/upIcon.png"
+export {default as cartIcon} from "./icons/cartIcon.png"
+export {default as filterIcon} from "./icons/filterIcon.png"
+export {default as addIcon} from "./icons/addIcon.png"
+export {default as minusIcon} from "./icons/minusIcon.png"
+export {default as emailIcon} from "./icons/emailIcon.png"
+export {default as passwordIcon} from "./icons/passwordIcon.png"
+export {default as userIcon} from "./icons/userIcon.png"
+
+
+
+
+export {default as cakeBg} from "./images/cakeBg.png"
+export {default as donutsBg} from "./images/donutsBg.png"
+export {default as berryBg} from "./images/berryBg.png"
+export {default as breadBg} from "./images/breadBg.png"
+export {default as breadBg2} from "./images/breadBg2.png"
+export {default as brownieBg} from "./images/brownieBg.png"
+export {default as discountPhoto} from "./images/discountPhoto.jpg"
+export {default as paymentsBg} from "./images/paymentsBg.png"
+export {default as aboutUsImg1} from "./images/aboutUsImg1.jpg"
+export {default as aboutUsImg2} from "./images/aboutUsImg2.jpg"
+export {default as loginBg1} from "./images/loginBg1.png"
+export {default as loginBg2} from "./images/loginBg2.png"
+export {default as loginBg3} from "./images/loginBg3.png"
+export {default as imagePlaceholder} from "./images/imagePlaceholder.png"
+
+
